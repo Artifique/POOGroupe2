4bff09b4b300ddb8cf0cb4f1fcdfe61fd335fc9f
saran1994 <59394975+saran1994@users.noreply.github.com>
2024-05-10 15:48:10 +0000

    Classe Evenement
    Premier commit

---
+++ event.js
@@ -0,0 +1,11 @@
+var evenement = /** @class */ (function () {
+    function evenement(id_event, id_lieu, id_org, titre, description) {
+        this.id_event = id_event;
+        this.id_lieu = id_lieu;
+        this.id_org = id_org;
+        this.titre = titre;
+        this.description = description;
+    }
+    return evenement;
+}());
+;
