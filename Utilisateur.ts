160844dbfff061cccfc561ce8a6a9dded35ee131
saran1994 <59394975+saran1994@users.noreply.github.com>
2024-05-13 15:45:21 +0000

    ClassUtilisateur

--- Utilisateur.ts
+++ Utilisateur.ts
@@ -28,7 +28,10 @@ export class Utilisateur{
     // private id_util:number;
     public nom_util:string;
     public email_util:string;
-    constructor(nom_u:string,email_u:string){
+    public num_util: number;
+    public mdp_util: string;
+
+    constructor(nom_u:string,email_u:string, num_u:string, mdp_u:string){
             // this.id_util=id_u;
             this.nom_util=nom_u;
             this.email_util=email_u;
@@ -38,10 +41,10 @@ export class Utilisateur{
     // }
     public CreerCompte(){
         // Requête d'insertion
-        const query = 'INSERT INTO utilisateur (nom_util,email_util) VALUES (?, ?)';
+        const query = 'INSERT INTO utilisateur (nom_util,email_util,num_util,mdp_util) VALUES (?,?,?,?)';
         
         // Exécution de la requête d'insertion
-        connection.query(query, [this.nom_util,this.email_util], (err, results) => {
+        connection.query(query, [this.nom_util,this.email_util,this.num_util,this.mdp_util], (err, results) => {
             if (err) {
                 console.error('Erreur lors de l\'insertion de l\'événement :', err);
                 return;
@@ -49,8 +52,28 @@ export class Utilisateur{
             console.log('Utilisateur inséré avec succès !');
             console.log('ID de l\'utilisateur inséré :', results.insertId);
         });
-
     }
+    
+    public ModifierUtilisateur(nom_util:string,email_util:string, num_util:number, mdp_util:string){
+        const sql = "UPDATE utilisateur SET nom_util = '"+nom_util+"', email_util = '"+email_util+"', num_util = '"+num_util+"', mdp_util = '"+mdp_util+"'  WHERE nom_util = '"+nom_util+"'";
+        connection.query(sql, function (err, result) {
+          if (err){
+          console.log("Erreur lors de la modification!!");
+          return;}
+          console.log("utilisateur Modifié !!");
+        });
+      } 
+//à ajouter dans gestionnaire.ts :
+public SupUtlisateur(email: string):void{
+  const sql = "DELETE FROM utilisateur WHERE email_util = '"+email+"'";
+  connection.query(sql, function (err, result) {
+    if (err){
+    console.log("Erreur lors de la suppression!!");
+    return;}
+    console.log("Utilisateur Supprimé !!");
+  });
+} 
+
     public RechercherEvent(nom_event:string):any{
        const query='SELECT * FROM evenement WHERE titre=?';
        connection.query(query, [nom_event], (err, results) => {
